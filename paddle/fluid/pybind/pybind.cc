7204fa39b59d2ee266fb35fc684283689b5e9639
Allen Guo <alleng@graphcore.ai>
2021-07-20 13:45:45 +0000

    revert code format not related with IPU

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -774,7 +774,7 @@ PYBIND11_MODULE(core_noavx, m) {
            py::arg("array"), py::arg("place"), py::arg("zero_copy") = false,
            R"DOC(
         Set the data of LoDTensor on place with given numpy array.
-
+        
         Args:
           lod (numpy.ndarray): The data to set.
           place (CPUPlace|CUDAPlace|XPUPlace|CUDAPinnedPlace|NPUPlace): The place where the
@@ -856,41 +856,41 @@ PYBIND11_MODULE(core_noavx, m) {
 
   // TODO(cql): add reference: en_user_guide_lod_tensor
   py::class_<LoDTensor, framework::Tensor>(m, "LoDTensor", R"DOC(
-    LoDTensor is a Tensor with optional LoD (Level of Details) information,
-    it can be used for variable-length sequences,
+    LoDTensor is a Tensor with optional LoD (Level of Details) information, 
+    it can be used for variable-length sequences, 
     see :ref:`user_guide_lod_tensor` for details.
 
     LoDTensor can be converted to numpy array using :code:`numpy.array(lod_tensor)`.
 
-    You can skip the following explanation if you don't need to know details
+    You can skip the following explanation if you don't need to know details 
     of LoDTensor.
 
-    The following two examples show how to use LODtensor to represent
+    The following two examples show how to use LODtensor to represent 
     variable-length sequences.
-
+    
     Example 1:
-
-    Suppose x is a LoDTensor representing a variable-length sequence.
-    It contains two logical subsequences, the length of first logical sequence
-    is 2 (e.g., number of samples is 2), the length of second logical sequence
-    is 3, and the total length is 5. The data of the first logical sequence is
-    [1, 2], [3, 4], and the data of the second logical sequence is [5, 6],
-    [7, 8], [9, 10]. The data dimension of each sample is 2. So, the final
-    shape of the LoDTensor is [5, 2], of which 5 is the total length and 2 is
+    
+    Suppose x is a LoDTensor representing a variable-length sequence. 
+    It contains two logical subsequences, the length of first logical sequence 
+    is 2 (e.g., number of samples is 2), the length of second logical sequence 
+    is 3, and the total length is 5. The data of the first logical sequence is 
+    [1, 2], [3, 4], and the data of the second logical sequence is [5, 6], 
+    [7, 8], [9, 10]. The data dimension of each sample is 2. So, the final 
+    shape of the LoDTensor is [5, 2], of which 5 is the total length and 2 is 
     the dimension of each sample.
-
-    Logically, we can represent the variable-length sequence in two ways: one
-    is in the form of recursive sequence lengths, that is,
-    x.recursive_sequence_lengths=[[2, 3]]; the other is in the form of offsets,
-    that is, x.lod=[[0, 2, 2+3]]. These two representations are equivalent, and
-    you can set and retrieve recursive_sequence_lengths or LoD through the
+    
+    Logically, we can represent the variable-length sequence in two ways: one 
+    is in the form of recursive sequence lengths, that is, 
+    x.recursive_sequence_lengths=[[2, 3]]; the other is in the form of offsets, 
+    that is, x.lod=[[0, 2, 2+3]]. These two representations are equivalent, and 
+    you can set and retrieve recursive_sequence_lengths or LoD through the 
     corresponding interfaces of LoDTensor introduced later.
 
-    Actually, in order to access sequence faster, Paddle uses offset to store
-    different lengths of sequences.
-    Therefore, the operations on recursive_sequence_lengths will be converted
+    Actually, in order to access sequence faster, Paddle uses offset to store 
+    different lengths of sequences. 
+    Therefore, the operations on recursive_sequence_lengths will be converted 
     to the operations on LoD eventually.
-
+    
     .. code-block:: python
 
       y.data = [[1, 2], [3, 4],
@@ -905,18 +905,18 @@ PYBIND11_MODULE(core_noavx, m) {
 
     Example 2:
 
-    LoD may have more than one level (for example, a paragraph may have more
-    than one sentence and a sentence may have more than one word). Suppose y
-    is a LoDTensor and its lod_level is 2.
-    From level = 0, there are two logical sequences, the length of which is
-    2 and 1, respectively, indicating that the first logical sequence contains
-    two sub-sequences and the second logical sequence contains one sub-sequence.
-    From level = 1, the lengths of two sub-sequences contained by the first
-    logical sequence is 2 and 2, and the length of sub-sequence contained by
+    LoD may have more than one level (for example, a paragraph may have more 
+    than one sentence and a sentence may have more than one word). Suppose y 
+    is a LoDTensor and its lod_level is 2. 
+    From level = 0, there are two logical sequences, the length of which is 
+    2 and 1, respectively, indicating that the first logical sequence contains 
+    two sub-sequences and the second logical sequence contains one sub-sequence. 
+    From level = 1, the lengths of two sub-sequences contained by the first 
+    logical sequence is 2 and 2, and the length of sub-sequence contained by 
     the second logical sequence is 3.
-
-    Therefore, the LoDTensor is represented in the form of recursive sequence
-    lengths as y.recursive_sequence_lengths=[[2,1], [2,2,3]]; and equally, in
+      
+    Therefore, the LoDTensor is represented in the form of recursive sequence 
+    lengths as y.recursive_sequence_lengths=[[2,1], [2,2,3]]; and equally, in 
     the form of offset, it is represented as y.lod=[[0,2,3], [0,2,4,7]].
 
     .. code-block:: python
@@ -1026,7 +1026,7 @@ PYBIND11_MODULE(core_noavx, m) {
 
            Args:
                 recursive_sequence_lengths (list[list[int]]): The recursive sequence lengths.
-
+           
            Returns:
                 None.
 
@@ -1056,7 +1056,7 @@ PYBIND11_MODULE(core_noavx, m) {
 
            Returns:
                list[list[int]]: The lod of the LoDTensor.
-
+           
            Examples:
                .. code-block:: python
 
@@ -1079,7 +1079,7 @@ PYBIND11_MODULE(core_noavx, m) {
              return new_lod;
            },
            R"DOC(
-           Return the recursive sequence lengths corresponding to of the LodD
+           Return the recursive sequence lengths corresponding to of the LodD 
            of the LoDTensor.
 
            Returns:
@@ -1337,7 +1337,7 @@ All parameter, weight, gradient are variables in Paddle.
       .def("find_var", &Scope::FindVar, py::arg("name"),
            R"DOC(
            Find variable named :code:`name` in the current scope or
-           its parent scope. Return None if not found.
+           its parent scope. Return None if not found. 
 
            Args:
                name (str): the variable name.
@@ -1466,12 +1466,12 @@ All parameter, weight, gradient are variables in Paddle.
         R"DOC(
              Prune the backward part of a program, mostly called in
              program.clone(for_test=True).
-
+              
              Args:
                    program (ProgramDesc): The original program.
 
              Returns:
-                   tuple(ProgramDesc, map<int, int>): The first part is
+                   tuple(ProgramDesc, map<int, int>): The first part is 
                    the pruned program desc, and the second part is a map
                    which contains the id pair of pruned block and corresponding
                    origin block.
@@ -2112,7 +2112,7 @@ All parameter, weight, gradient are variables in Paddle.
            },
            py::arg("tensor"), R"DOC(
              Append a LoDensor to LoDTensorArray.
-
+              
              Args:
                    tensor (LoDTensor): The LoDTensor to be appended.
 
@@ -2455,9 +2455,9 @@ All parameter, weight, gradient are variables in Paddle.
                 Default 100.
 
                 .. note::
-                    1. If you fetch data when calling the 'run', the ParallelExecutor
-                    will clean up the temp variables at the end of the current iteration.
-                    2. In some NLP model, it may cause the GPU memory is insufficient,
+                    1. If you fetch data when calling the 'run', the ParallelExecutor 
+                    will clean up the temp variables at the end of the current iteration. 
+                    2. In some NLP model, it may cause the GPU memory is insufficient, 
                     in this case, you should reduce `num_iteration_per_drop_scope`.
 
                 Examples:
@@ -2971,7 +2971,7 @@ All parameter, weight, gradient are variables in Paddle.
                 synchronous batch normalization which synchronizes the mean
                 and variance through multi-devices in training phase.
                 Current implementation doesn't support FP16 training and CPU.
-                And only synchronous on one machine, not all machines.
+                And only synchronous on one machine, not all machines. 
                 Default is False.
 
                 Examples:
@@ -3009,9 +3009,9 @@ All parameter, weight, gradient are variables in Paddle.
           R"DOC((bool, optional): memory opitimize aims to save total memory
                 consumption, set to True to enable it.
 
-                Default None. None means framework would choose to use or not use
-                this strategy automatically. Currently, None means that it is
-                enabled when GC is disabled, and disabled when GC is enabled.
+                Default None. None means framework would choose to use or not use 
+                this strategy automatically. Currently, None means that it is 
+                enabled when GC is disabled, and disabled when GC is enabled. 
                 True means enabling and False means disabling. Default is None.
 
                 Examples:
@@ -3024,7 +3024,7 @@ All parameter, weight, gradient are variables in Paddle.
 
                         build_strategy = static.BuildStrategy()
                         build_strategy.memory_optimize = True
-
+                
                 )DOC")
       .def_property(
           "is_distribution",
